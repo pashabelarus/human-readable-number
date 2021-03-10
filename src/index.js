87fe0204547a11539f9f1ae7692736d554fa41f9
Pavel Suntsou <pasha2010.94@gmail.com>
2021-03-11 01:20:22 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function toReadable (number) {
-  
+const a = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+const b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+
+const toReadable = (num) => {
+    if (num === 0) return 'zero';
+    if ((num = num.toString()).length > 9) return 'overflow';
+    const n = ('0000000' + num).substr(-9).match(/(\d{2})(\d{1})(\d{2})$/);
+    if (!n) return; var str = '';
+    str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'thousand ' : '';
+    str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'hundred ' : '';
+    str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) : '';
+
+    return str.trim();
 }
+
+module.exports = toReadable
